64d912291fbee64097ceea1ed4c14f6849e0e102
SHADIL VV <shadilshad508@gmail.com>
2025-12-16 23:26:07 +0530

    while loop

--- whilee.java
+++ whilee.java
@@ -1,16 +1,18 @@
-public class while{
+import java.util.Scanner;
+public class whilee{
     public static void main(String []args){
+        Scanner sc =  new Scanner(System.in);
 
-
+            int m= sc.nextInt();
         /* while (condition) {
     // code to repeat
 }
  */
-        int m=9;
-        while(m<=10){
+        while(m<=100){
             System.out.println("The value can be : " + m);
             m++;
         }
     }
+
 
 }
