6a8b131677c0b5391ca8668593c7e4dbf92df929
SHADIL VV <shadilshad508@gmail.com>
2025-12-16 20:16:22 +0530

    datatypes and variables

---
+++ variables.java
@@ -0,0 +1,40 @@
+/* A variable is a named memory location that holds data your program works with.
+
+
+
+dataType variableName = value;
+
+
+*/
+
+
+class Variables{
+    public static void main(String []args){
+
+
+/*| Type      | Size    | Used for                    |
+| --------- | ------- | --------------------------- |
+| `byte`    | 1 byte  | very small numbers          |
+| `short`   | 2 bytes | small numbers               |
+| `int`     | 4 bytes | whole numbers               |
+| `long`    | 8 bytes | very large numbers          |
+| `float`   | 4 bytes | decimal numbers             |
+| `double`  | 8 bytes | decimal numbers (preferred) |
+| `char`    | 2 bytes | single character            |
+| `boolean` | 1 bit   | true / false                |
+
+non-primitive : String name = null;
+
+*/
+        short u= 1;
+        int userid =  101;
+        float me=3.1f;
+
+        System.out.println(u);
+        System.out.println("my userid is "+ userid);
+        System.out.println(me);
+
+
+    }
+    
+}
