aeadb5caec8b9d1f939226b653da41f577b94b7d
SHADIL VV <shadilshad508@gmail.com>
2025-12-20 18:39:47 +0530

    class and objects in java

---
+++ second.java
@@ -0,0 +1,14 @@
+class second{
+    public static void main(String[] args){
+        Classandobjects myobject = new Classandobjects();
+        System.out.println(myobject.d);
+        Classandobjects myproject = new Classandobjects();
+        Classandobjects myname = new Classandobjects();
+        System.out.println("so therefore "+ myproject.z);
+        System.out.println("And his name is "+ myname.name
+         + "And the numbers are "+ myproject.z + " and "
+         + myproject.d);
+
+
+    } 
+}
