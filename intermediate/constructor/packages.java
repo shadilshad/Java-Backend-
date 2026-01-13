2953d25731bfc3e8420f008f8412f6266c98ed75
SHADIL VV <shadilshad508@gmail.com>
2026-01-13 17:58:31 +0530

    package

---
+++ intermediate/constructor/packages.java
@@ -0,0 +1,13 @@
+public class packages {
+    
+
+
+
+// | Package     | Use                                 
+// `java.lang` | Core classes (String, Math, System) 
+// `java.util` | Scanner, ArrayList, Date            |
+// `java.io`   | File handling                       |
+// `java.sql`  | Database                            |
+// `java.net`  | Networking                          |
+
+}
