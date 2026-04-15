9ef9655ebee3028d90abc36fdb50a0ece945c4eb
SHADIL VV <shadilshad508@gmail.com>
2026-04-15 14:43:10 +0530

    Class and Object

---
+++ Level1_phase1/Project_Team/ClassandObjectt.java
@@ -0,0 +1,11 @@
+public class ClassandObjectt {
+    String name = "SHADIL VV";
+    int age = 10;
+
+    public static void main(String[] args) {
+        
+        ClassandObjectt co = new ClassandObjectt();
+        System.out.println(co.name + " is " + co.age + " years old ");
+    }
+
+}
